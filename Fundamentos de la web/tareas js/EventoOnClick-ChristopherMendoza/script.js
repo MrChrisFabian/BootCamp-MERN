f9eda286832f7336343985991629581e163e9524
MrChrisFabian <christopher.mendoza@fiuni.edu.py>
2024-01-27 15:24:24 -0300

    terminado

--- Fundamentos de la web/tareas js/EventoOnClick-ChristopherMendoza/script.js
+++ Fundamentos de la web/tareas js/EventoOnClick-ChristopherMendoza/script.js
@@ -2,7 +2,8 @@ const BotLogin = document.getElementById("sesion")
 const BotAdd = document.getElementById("Agregar")
 const BotLike = document.getElementById("megusta-1")
 const BotLike2 = document.getElementById("megusta-2")
-
+let likes1 = 14;
+let likes2 = 50;
 BotLogin.addEventListener("click", () => {
     BotLogin.innerHTML = "Logout";
 });
@@ -12,10 +13,14 @@ BotAdd.addEventListener("click",()=>{
 })
 
 BotLike.addEventListener("click", () => {
+    likes1+=1;
+    BotLike.innerText = likes1 + " Likes";
     alert("Ninja was liked");
 });
 
 BotLike2.addEventListener("click", () => {
+    likes2+=1;
+    BotLike2.innerText = likes2 + " Likes";
     alert("Ninja was liked");
 });
 
